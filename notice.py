2674c338c2732a26384d1fb05b6daae61ae7284d
Tran-Nam <phuongnam9613@gmail.com>
2019-05-22 16:05:43 +0700

    use D.txt as input to notice

--- notice.py
+++ notice.py
@@ -1,5 +1,8 @@
 from google_speech import Speech
 import os
+import time
+
+
 def notice(text, lang='vi'):
     try:
         speech = Speech(text, lang)
@@ -8,16 +11,21 @@ def notice(text, lang='vi'):
         print('No network connect! So i can not speak for you!')
 
 # use file D.txt as input to speech
-def notice(path='data', filename='D.txt', lang='vi'):
+def notice_by_speech(path='data', filename='D.txt', lang='vi'):
     if filename in os.listdir(path):
+
+        # reach content file
         abs_path = os.path.join(path, filename)
         content = open(abs_path, 'r').read()
-        os.remove(abs_path)
-        print(content)
-        try:
-            speech = Speech(content, lang)
-            speech.play()
-        except Exception:
-            print('No network connect! So i can not speak for you!')
 
-# read_txt()
+        if content is None or content=="":
+            return
+        # empty file
+        with open(abs_path, 'w') as f:
+            f.write("")
+        # print(content)
+        notice(content)
+
+while True:
+    notice_by_speech()
+    time.sleep(0.5)
