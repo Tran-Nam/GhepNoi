1337de1bb33ff6fd1dfcec97e7e59130465b30d4
Tran-Nam <phuongnam9613@gmail.com>
2019-05-23 21:10:33 +0700

    simple code

--- notice.py
+++ notice.py
@@ -8,22 +8,23 @@ def notice(text, lang='vi'):
         speech = Speech(text, lang)
         speech.play()
     except Exception:
+        print(text)
         print('No network connect! So i can not speak for you!')
 
 # use file D.txt as input to speech
 def notice_by_speech(path='data', filename='D.txt', lang='vi'):
     if filename in os.listdir(path):
-
         # reach content file
         abs_path = os.path.join(path, filename)
         content = open(abs_path, 'r').read()
+
 
         if content is None or content=="":
             return
         # empty file
         with open(abs_path, 'w') as f:
             f.write("")
-        # print(content)
+
         notice(content)
 
 while True:
